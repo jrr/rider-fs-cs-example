91a9fa58483f43bb4349a092a5a9a890fc81336e
John Ruble <johnruble@gmail.com>
2018-04-24 22:40:03 -0400

    example

---
+++ cs-netstandard/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace cs_netstandard
+{
+    public class Class1
+    {
+        public string foo;
+    }
+}
---
+++ fs-netframework/Library.fs
@@ -0,0 +1,7 @@
+namespace fs_netframework
+
+module Say =
+    let hello name =
+        let x = new cs_netstandard.Class1()
+        x.foo <- "jkl"
+        printfn "Hello %s" name
